f8bfcc1e436c96da68a2b9dcc6b5174acf1b030f
someone <someone@someplace.com>
2022-02-11 19:13:57 +0700

    up

--- js/link.js
+++ js/link.js
@@ -1,9 +1,12 @@
 document.getElementById('C++').addEventListener('click', function() {
     location.href = './C++.html'
 });
-document.getElementById('Python').addEventListener('click', function() {
-    location.href = './Python.html'
+document.getElementById('dang-ky-hoc').addEventListener('click', function() {
+    location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSfHRyRaHyFv2rsyH-De-_jAP0hFcybDSuIsq70ZoT8eAVeLgQ/viewform'
 });
-document.getElementById('Java').addEventListener('click', function() {
-    location.href = './Java.html'
-});
+// document.getElementById('Python').addEventListener('click', function() {
+//     location.href = './Python.html'
+// });
+// document.getElementById('Java').addEventListener('click', function() {
+//     location.href = './Java.html'
+// });
