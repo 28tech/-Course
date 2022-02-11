e6ec555ddf26e1559ac22fe7b0a0c4a9908ff3e9
someone <someone@someplace.com>
2022-02-11 11:59:01 +0700

    up

---
+++ js/link.js
@@ -0,0 +1,9 @@
+document.getElementById('C++').addEventListener('click', function() {
+    location.href = './C++.html'
+});
+document.getElementById('Python').addEventListener('click', function() {
+    location.href = './Python.html'
+});
+document.getElementById('Java').addEventListener('click', function() {
+    location.href = './Java.html'
+});
