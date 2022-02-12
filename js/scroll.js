4ca0ac77b732c99888fcb3c6dd3dfb1819cf1d17
someone <someone@someplace.com>
2022-02-12 11:38:33 +0700

    up

--- js/scroll.js
+++ js/scroll.js
@@ -15,8 +15,39 @@ scrollBtn.addEventListener('click' , () => {
         behavior: "smooth"
     })
 })
-$("#button").click(function() {
+$("#t1").click(function() {
     $('html, body').animate({
-        scrollTop: $("#myDiv").offset().top
-    }, 1000);
+        scrollTop: $("#div1").offset().top-80
+    }, 300);
 });
+$("#t2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div2").offset().top-80
+    }, 300);
+});
+$("#t3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div3").offset().top-80
+    }, 300);
+});
+$("#t4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div4").offset().top-80
+    }, 300);
+});
+$("#t5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div5").offset().top-80
+    }, 300);
+});
+$("#t6").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div6").offset().top-80
+    }, 300);
+});
+$("#t7").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#div7").offset().top-80
+    }, 300);
+});
+
